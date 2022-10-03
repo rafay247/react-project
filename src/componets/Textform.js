4947f6c55fb4726c0951a52ba0395830efe0b12a
rafay <rafayf3@gmail.com>
2022-10-03 20:39:11 +0545

    add exrtra logic

--- src/componets/Textform.js
+++ src/componets/Textform.js
@@ -14,22 +14,22 @@ export default function Textform(props) {
         setText(event.target.value)
 
     }
-    const [text, setText] = useState("Enter the text")
+    const [text, setText] = useState("")
     return (
         <>
-        <div className='container'>
-            <h2>{props.heading}</h2>
+        <div className='container my-3' >
+            <h1>{props.heading}</h1>
             <div className="mb-4">
-                <textarea className="form-control"value ={text} onChange = {handleOnChange} id="myBox" rows="8"></textarea>
+                <textarea className="form-control"value ={text} onChange = {handleOnChange} id="myBox" rows="6"></textarea>
             </div>
-            <button className='btn btn-primary mx-1' onClick={handleUpClick}>Convert to Uppercase</button>
-            <button className='btn btn-primary mx-1' onClick={handleLoClick}>Convert to Lowercase</button>
+            <button className='btn btn-success mx-1' onClick={handleUpClick}>Convert to Uppercase</button>
+            <button className='btn btn-success mx-1' onClick={handleLoClick}>Convert to Lowercase</button>
         </div>
-        <di className="container my-3">
-            <h1>Your text summery</h1>
-            <p>{text.split(' ').length} words {text.length} character</p>
-            <p>{0.008 * text.split(' ').length} Minutes read</p>
-            <h2>Preview</h2>
+        <di className="container my-2">
+            <h2>Your text summery</h2>
+            <p>{text.split(' ').length -1 } words {text.length} character</p>
+            <p>{0.008 * (text.split(' ').length - 1)} Minutes read</p>
+            <h3>Preview</h3>
             <p>{text}</p>
         </di>
         </>
