7fa27b4683db74e703bbc961ae2904680eaaa289
rafay <rafayf3@gmail.com>
2022-10-05 18:06:09 +0545

    creating dark mode light mode

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import { useState } from "react";
 import "./App.css";
 // import About from "./componets/About";
 import Navbar from "./componets/Navbar";
 import Textform from "./componets/Textform";
 
 function App() {
+
+  const [mode, setMode] = useState('Light');
+
   return (
   <>
   <Navbar title="TextUtils" about="textutils about" />
