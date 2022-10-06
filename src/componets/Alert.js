96316a4e838b8e06077343ed888f214e19c143f8
rafay <rafayf3@gmail.com>
2022-10-06 22:58:26 +0545

    alert

--- src/componets/Alert.js
+++ src/componets/Alert.js
@@ -1,9 +1,14 @@
 import React from 'react'
 
-export default function Alert() {
+export default function Alert(props) {
+
+    const capitalize = (word) => {
+        const lower = word.toLowerCase();
+        return lower.charAt(0).toUpperCase() + lower.slice(1);
+    }
     return (
-        <div className="alert alert-warning alert-dismissible fade show" role="alert">
-            <strong>Holy guacamole!</strong> You should check in on some of those fields below.
+        <div className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
+            <strong>{capitalize(props.alert.type)}</strong> {props.alert.message}
             <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
         </div>
     )
