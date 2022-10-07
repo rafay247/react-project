6e84ed7c1a51fd75b71c927d7f530d36c61eba0d
rafay <rafayf3@gmail.com>
2022-10-07 18:00:20 +0545

    react routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { useState } from 'react'
 import "./App.css";
-// import About from "./componets/About";
+import About from "./componets/About";
 import Navbar from "./componets/Navbar";
 import Textform from "./componets/Textform";
 import Alert from "./componets/Alert";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom"
 
 function App() {
 
-  const [mode, setMode] = useState('Light');
+  const [mode, setMode] = useState('');
   const [alert, setAlert] = useState(null)
 
   const showAlert = (message, type) => {
@@ -21,26 +22,34 @@ function App() {
   }, 2500);
 
   const toggleMode = () => {
-    if (mode === 'light') {
-      setMode('dark');
-      document.body.style.backgroundColor = 'black';
-      showAlert("Dark mode has been eanabled", 'success')
-      document.title = "TextUtils - Dark mode";
-    }
-    else {
+    if (mode === 'dark') {
       setMode('light');
       document.body.style.backgroundColor = 'white';
       showAlert("Light mode has been eanabled", 'success')
       document.title = "TextUtils - Light mode";
+    }
+    else {
+      setMode('dark');
+      document.body.style.backgroundColor = 'black';
+      showAlert("Dark mode has been eanabled", 'success')
+      document.title = "TextUtils - Dark mode";
 
     }
   };
   return (
     <>
-      <Navbar title="TextUtils" about="textutils about" mode={mode} toggleMode={toggleMode} />
-      <Alert alert={alert} />
-      <Textform showAlert={showAlert} heading="Enter the text to analyze below" mode={mode} />
-      {/* <About/> */}
+      <Router>
+        <Navbar title="TextUtils" about="textutils about" mode={mode} toggleMode={toggleMode} />
+        <Alert alert={alert} />
+        <Switch>
+          <Route path="/">
+          <Textform showAlert={showAlert} heading="Enter the text to analyze below" mode={mode} />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+        </Switch>
+      </Router>
     </>
   )
 }
