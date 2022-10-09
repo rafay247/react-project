a0a2e0026aa10778e051790f2804b1056de795d0
rafay <rafayf3@gmail.com>
2022-10-09 17:03:30 +0545

    wrapping textUtils

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { useState } from 'react'
 import "./App.css";
-import About from "./componets/About";
+// import About from "./componets/About";
 import Navbar from "./componets/Navbar";
 import Textform from "./componets/Textform";
 import Alert from "./componets/Alert";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 
 function App() {
 
-  const [mode, setMode] = useState('');
+  const [mode, setMode] = useState('light');
   const [alert, setAlert] = useState(null)
 
   const showAlert = (message, type) => {
@@ -37,16 +37,17 @@ function App() {
   };
   return (
     <>
-      <Router>
+      {/* <Router> */}
         <Navbar title="TextUtils" about="About" mode={mode} toggleMode={toggleMode} />
         <Alert alert={alert} />
-
-        <Routes>
+        <Textform showAlert={showAlert} heading="Enter the text to analyze" mode={mode} />
+        
+        {/* <Routes>
           <Route exact path="about" element={<About mode={mode} />} />
           <Route exact path="/" element={<Textform showAlert={showAlert} heading="Enter the text to analyze" mode={mode} />} />
         </Routes>
 
-      </Router>
+      </Router> */}
     </>
   )
 }
